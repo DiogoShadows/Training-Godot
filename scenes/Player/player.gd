01350320dc0fb3b23438224b468f68067f5a7ddb
Diogo Santos <diogo_f_s_@hotmail.com>
2024-02-04 19:16:36 -0300

    Changes

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+signal player_shoot_laser(pos : Vector2, direction : Vector2)
+signal player_throw_grenade(pos : Vector2, direction : Vector2)
+
+var can_laser: bool = true
+var can_grenade: bool = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -7,11 +12,31 @@ func _process(_delta):
 	var direction : Vector2 = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * 500
 	
+	#Rotate Player
+	look_at(get_global_mouse_position())
+	
 	#Shoot
-	if Input.is_action_just_pressed("primary action"):
-		print("Shoot")
+	if Input.is_action_just_pressed("primary action") and can_laser:
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
+		player_shoot_laser.emit(selected_laser.global_position, GetPlayerDirection())
+		can_laser = false
+		$Laser_timer.start()
 		
-	elif Input.is_action_just_pressed("secondary action"):
-		print("Shoot granade")
+	elif Input.is_action_just_pressed("secondary action") and can_grenade:
+		var grenade_marker = $GrenadeStartPositions/Marker2D
+		player_throw_grenade.emit(grenade_marker.global_position, GetPlayerDirection())
+		can_grenade = false
+		$Granade_timer.start()
 		
 	move_and_slide()
+
+func GetPlayerDirection() -> Vector2:
+	return (get_global_mouse_position() - position).normalized()
+	
+func _on_laser_timer_timeout():
+	can_laser = true
+
+
+func _on_granade_timer_timeout():
+	can_grenade = true
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,21 @@
 extends Node2D
+
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene : PackedScene = preload("res://scenes/projectiles/Grenade.tscn")
+
+func _on_gate_player_entered_gate(body):
+	print(body)
+
+
+func _on_player_player_shoot_laser(pos : Vector2, direction : Vector2):
+	var laser = laser_scene.instantiate()
+	laser.position = pos
+	laser.laserDirection = direction
+	laser.rotation_degrees = rad_to_deg(direction.angle())
+	$Projectiles.add_child(laser)
+
+func _on_player_player_throw_grenade(pos : Vector2, direction : Vector2):
+	var grenade = grenade_scene.instantiate() as RigidBody2D
+	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.SPEED
+	$Projectiles.add_child(grenade)
